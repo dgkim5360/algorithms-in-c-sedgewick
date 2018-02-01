49b773d9f924b625c147032f3e036421031e26f9
Don Kim <dgkimdev@gmail.com>
2018-02-01 15:49:38 +0900

    Polish Section 5.2. Recursion and Trees

--- part2-data-structures/chapter5-recursion-and-trees/program07-towers-of-hanoi.c
+++ part2-data-structures/chapter5-recursion-and-trees/program07-towers-of-hanoi.c
@@ -1,10 +1,12 @@
 /* Program 5.7: Solution to the towers of Hanoi
- * We shift the tower of disks to the right by (recusively) shifting all but
- * the bottom disk to the left, then shifting the bottom disk to the right,
- * then (recursively) shifting the tower back onto the bottom disk.
+ * We shift the tower of disks to the right by (recusively) shifting
+ * all but the bottom disk to the left, then shifting the bottom disk
+ * to the right, then (recursively) shifting the tower back onto the
+ * bottom disk.
  *
  * Property 5.2: The recursive divide-and-conquer algorithm for the
- * towers of Hanoi problem produces a solution that has 2^N-1 moves.  */
+ * towers of Hanoi problem produces a solution that has 2^N-1 moves.
+ */
 void hanoi(int N, int d) {
   if (N == 0)
     return;
--- part2-data-structures/chapter5-recursion-and-trees/program09-nonrecursive-program-to-draw-a-ruler.c
+++ part2-data-structures/chapter5-recursion-and-trees/program09-nonrecursive-program-to-draw-a-ruler.c
@@ -1,10 +1,11 @@
 /* Program 5.9: Nonrecursive program to draw a ruler
- * In contrast to Program 5.8, we can also draw a ruler by first drawing all
- * the marks of length 1, then drawing all the marks of length 2, and so forth.
- * The variable `t` carries the length of the marks and the variable `j`
- * carries the number of marks in between two successive marks of length `t`.
- * The outer `for` loop increments `t` and preserves the property j = 2^(t-1).
- * The inner `for` loop draws all the marks of length `t`.
+ * In contrast to Program 5.8, we can also draw a ruler by first
+ * drawing all the marks of length 1, then drawing all the marks of
+ * length 2, and so forth. The variable `t` carries the length of the
+ * marks and the variable `j` carries the number of marks in between two
+ * successive marks of length `t`. The outer `for` loop increments `t`
+ * and preserves the property j = 2^(t-1). The inner `for` loop draws
+ * all the marks of length `t`.
  */
 void rule(int l, int r, int h) {
   int i, j, t;
