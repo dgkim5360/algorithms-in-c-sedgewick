c4f1b30a649fdc8cb53a64328b310c1784022b96
Don Kim <dgkimdev@gmail.com>
2018-04-26 16:18:08 +0900

    Edit the indentation

--- part3-sorting/chapter06-elementary-sorting-methods/program01-example-array-sort-with-driver.c
+++ part3-sorting/chapter06-elementary-sorting-methods/program01-example-array-sort-with-driver.c
@@ -30,29 +30,29 @@ typedef int Item;
 #define compexch(A, B) if (less(B, A)) exch(A, B)
 
 void sort(Item a[], int l, int r) {
-  int i, j;
+	int i, j;
 
-  for (i = l+1; i <= r; ++i)
-    for (j = i; j > l; --j)
-      compexch(a[j-1], a[j]);
+	for (i = l+1; i <= r; ++i)
+		for (j = i; j > l; --j)
+			compexch(a[j-1], a[j]);
 }
 
 int main(const int argc, char *argv[]) {
-  int i, N = atoi(argv[1]), sw = atoi(argv[2]);
-  int *a = malloc(N*sizeof(int));
+	int i, N = atoi(argv[1]), sw = atoi(argv[2]);
+	int *a = malloc(N*sizeof(int));
 
-  if (sw)
-    for (i = 0; i < N; ++i)
-      a[i] = 1000*(1.0*rand()/RAND_MAX);
-  else
-    while (scanf("%d", &a[N]) == 1)
-      ++N;
+	if (sw)
+		for (i = 0; i < N; ++i)
+			a[i] = 1000*(1.0*rand()/RAND_MAX);
+	else
+		while (scanf("%d", &a[N]) == 1)
+			++N;
 
-  sort(a, 0, N-1);
+	sort(a, 0, N-1);
 
-  for (i = 0; i < N; ++i)
-    printf("%3d ", a[i]);
-  printf("\n");
-  free(a);
-  return 0;
+	for (i = 0; i < N; ++i)
+		printf("%3d ", a[i]);
+	printf("\n");
+	free(a);
+	return 0;
 }
